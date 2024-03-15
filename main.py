24010ab3ddf8b0ed00876fce0de6540958ecb0c2
HOO JIAN LE <79522385+jianlel@users.noreply.github.com>
2024-03-15 09:36:44 +0800

    updated main.py to include the server side code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import os
 
 import Helper.settings as settings
 from Algorithm.Environment import staticEnvironment
@@ -8,8 +9,66 @@ from Helper import obstacleGenerator
 from Helper.constants import MOVEMENT
 from Simulator import Simulator
 
+import json
+import time
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+#from model import *
 
-def main():
+app = Flask(__name__)
+CORS(app)
+#model = load_model()
+model = None
+@app.route('/status', methods=['GET'])
+def status():
+    """
+    This is a health check endpoint to check if the server is running
+    :return: a json object with a key "result" and value "ok"
+    """
+    return jsonify({"result": "ok"})
+
+
+@app.route('/', methods=['POST'])
+def path_finding():
+    
+    """
+    This is the main endpoint for the path finding algorithm
+    :return: a json object with a key "data" and value a dictionary with keys "distance", "path", and "commands"
+    """
+    # Get the json data from the request
+    content = request.json
+    
+    #obstacle_data = json.loads(content)
+
+    """  	
+    obstacles = content['obstacles']
+    obs = obstacleGenerator.getTestObstacles()
+    obs1 = obstacleGenerator.getTestObstacles1()
+    obs2 = obstacleGenerator.getTestObstacles2()
+    obs3 = obstacleGenerator.getTestObstacles3()
+    obs4 = obstacleGenerator.getTestObstacles4()
+    obs5 = obstacleGenerator.getTestObstacles5()
+    obsTest = obstacleGenerator.getTestObstaclesTest()
+    #userInputObs = obstacleGenerator.getObstaclesThroughUserInput()
+    inputTxtObs = obstacleGenerator.getObstaclesThroughTxt()
+    """
+
+    jsonObstacles = obstacleGenerator.getObstaclesThroughJson(content)
+    
+    print("test1")
+    sim = Simulator(staticEnvironment((200, 200), jsonObstacles), jsonObstacles, False)   
+    sim.initialize_without_simulator()
+    #sim.initialize()
+    #sim.run()
+    print("test2")
+    
+    with open("log.txt" , "r") as file:
+        command_string = file.readline().strip()
+
+    return command_string
+    
+
+def simulation():
     obs = obstacleGenerator.getTestObstacles()
     obs1 = obstacleGenerator.getTestObstacles1()
     obs2 = obstacleGenerator.getTestObstacles2()
@@ -20,25 +79,12 @@ def main():
     #userInputObs = obstacleGenerator.getObstaclesThroughUserInput()
     inputTxtObs = obstacleGenerator.getObstaclesThroughTxt()
 
-    sim = Simulator(staticEnvironment((200, 200), inputTxtObs), inputTxtObs, False)   
+    sim = Simulator(staticEnvironment((400, 400), obs5), obs5, False)   
+    #sim.initialize_without_simulator()
     sim.initialize()
     sim.run()
-
-    """
-    with open("log.txt" , "r") as file:
-        command_string = file.readline().strip()
-
-    return command_string
-    """
+    
 
 if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
-
-
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    simulation()
